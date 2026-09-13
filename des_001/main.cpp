bd602111e9bdc8c63eb442cb4149c4f493a348b4
sebastianrodriguez8-art <sebastian.rodriguez8@udea.edu.co>
2026-09-12 19:42:30 -0500

    Informe
    Actualizacion del informe y solo un cambio para reducir la sintaxis del main

--- des_001/main.cpp
+++ des_001/main.cpp
@@ -1,6 +1,8 @@
 #include "funciones.h"
 #include <ctime>
 
+using namespace std;
+
 int main()
 {
     srand(time(NULL));
@@ -12,22 +14,22 @@ int main()
     unsigned char* tablero = crear_tablero(filas, cols, bytes_reservados);
     inicializar_tablero_aleatorio(tablero, filas, cols);
 
-    std::cout << "Bytes reservados: " << bytes_reservados << "\n\n";
+    cout << "Bytes reservados: " << bytes_reservados << "\n\n";
 
     // Mostrar el tablero leído bit a bit
-    std::cout << "Tablero inicial:\n";
+    cout << "Tablero inicial:\n";
     for (int f = 0; f < filas; ++f) {
         for (int c = 0; c < cols; ++c) {
             int valor = obtener_ficha(tablero, f, c, cols);
-            std::cout << valor << " ";
+            cout << valor << " ";
         }
-        std::cout << "\n";
+        cout << "\n";
     }
 
     // Modificar una casilla específica (ejemplo: poner la ficha 5 en la fila 1, columna 2)
     fijar_ficha(tablero, 1, 2, cols, 5);
 
-    std::cout << "\nValor modificado en (1, 2): " << (int)obtener_ficha(tablero, 1, 2, cols) << "\n";
+    cout << "\nValor modificado en (1, 2): " << (int)obtener_ficha(tablero, 1, 2, cols) << "\n";
 
     delete[] tablero;
     return 0;
